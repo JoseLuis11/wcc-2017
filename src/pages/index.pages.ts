e425fcacd2f0023e5f15b8acdac305d96caa031c
IsabelZR <izavala@cetys.edu.mx>
2017-10-06 18:20:58 -0700

    userProfile

--- src/pages/index.pages.ts
+++ src/pages/index.pages.ts
@@ -2,3 +2,4 @@ export { RegisterPage } from './register/register';
 export { LoginPage } from './login/login';
 export { HomePage } from '../pages/home/home';
 export { ListPage } from '../pages/list/list';
+export {UserProfilePage} from '../pages/user-profile/user-profile';
