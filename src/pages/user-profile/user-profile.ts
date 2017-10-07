8318e679245ff954d6a3001dca7bc469ecff8cbc
JoseLuis <jluis.scooter11@hotmail.com>
2017-10-06 21:12:49 -0700

    profile retrieving data

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -1,25 +1,33 @@
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the UserProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-user-profile',
   templateUrl: 'user-profile.html',
 })
 export class UserProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  profile;
+  name: string;
+  email: string;
+  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth, private afDb: AngularFireDatabase) {
   }
 
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad UserProfilePage');
+    this.afAuth.authState.subscribe(data => {
+      this.profile = this.afDb.object(`/users/${data.uid}`).valueChanges();
+      this.email = data.email;
+      console.log(this.profile);
+    });
   }
+
+    
+
+  
 
 }
